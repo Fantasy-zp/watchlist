a83cd0ff3fca9c933be10b03203ab27ab797ec40
fzp <546362652@qq.com>
2022-03-26 15:22:32 +0800

    User authentication with Flask-Login

--- 1.py
+++ 1.py
@@ -1,16 +1,6 @@
-from turtle import title
+from werkzeug.security import generate_password_hash, check_password_hash
+pw_hash = generate_password_hash('dog')  # 为密码 dog 生成密码散列值
+print(pw_hash)  # 查看密码散列值
 
-
-print(len('大萨达'))
-
-
-title = '3'
-year = '3'
-print(title == None)
-if not title or not year or len(year) != 4 or len(title) > 60:
-    print('Invalid input.')  # 显示错误提示
-else:
-    print("good!")
-
-if title:
-    print(True)
+print(check_password_hash(pw_hash, 'dog'))  # 检查散列值是否对应密码 dog
+print(check_password_hash(pw_hash, 'cat'))  # 检查散列值是否对应密码 cat
